1fc2843de24d81c1f477838b6df1f8c5dce80f0b
dde <dde@eldar-wrk.(none)>
2014-12-26 18:45:10 +0300

    dfs stub added

---
+++ dfs.c
@@ -0,0 +1,5 @@
+/* @author Denis Deryugin
+ * @date 26 Dec 2014
+ */
+
+
---
+++ dfs.h
@@ -0,0 +1,14 @@
+/* @author Denis Deryugin
+ * @date 26 Dec 2014
+ */
+
+#ifndef _DFS_H_
+#define _DFS_H_
+
+struct inode {
+	int num;
+	int page_start;
+	int len;
+};
+
+
