b816050e0db6b1b01e1eee3dfb5fe829ccda3333
Deryugin <deryugin.denis@gmail.com>
2014-12-29 18:01:13 +0300

    inode initialization

--- dfs.c
+++ dfs.c
@@ -7,6 +7,8 @@
 
 #define DFS_VERBOSE
 
+static struct dfs_superblock dfs_sb;
+
 int dfs_read_superblock(void) {
 	
 }
@@ -21,19 +23,33 @@ int dfs_read_inodes(void) {
 char *static_files[] = {"deadbeef", "cafebabe"};
 
 int dfs_init(void) {
-	int pt, file_pt;
+	int page_pt, file_pt;
 	char buff[DFS_BUFF_SIZE];
 	printf("Writing DFS image...\n");
 
 	for (int i = 0; i < NAND_BLOCK_COUNT; i++)
 		_block_erase(i);
 
-	struct dfs_superblock sb = {	sizeof(sb),
-					sizeof(static_files) / sizeof(static_files[0]) };
+	dfs_sb.sb_size = sizeof(dfs_sb);
+	dfs_sb.inode_count = sizeof(static_files) / sizeof(static_files[0]);
+
 	memset(buff, 0, DFS_BUFF_SIZE);
-	memcpy(buff, &sb, sizeof(sb));
-	for (int i = 0; i < page_from_pos(sizeof(sb) + NAND_PAGE_SIZE - 1); i++)
+	memcpy(buff, &dfs_sb, sizeof(dfs_sb));
+	page_pt = page_from_pos(sizeof(dfs_sb) + NAND_PAGE_SIZE - 1);
+	for (int i = 0; i < page_pt; i++)
 		_program_page(i, buff + i * NAND_PAGE_SIZE);
+	
+	file_pt = page_pt + dfs_sb.inode_count * (page_from_pos(sizeof(struct dfs_inode) + NAND_PAGE_SIZE - 1));
+	for (int i = 0; i < dfs_sb.inode_count; i++) {
+		printf("\tAdding file: %s\n", static_files[i]);
+
+		int file_len = page_from_pos(strlen(static_files[i] + NAND_PAGE_SIZE - 1));
+		struct dfs_inode node = { i, file_pt, file_len};
+		memset(buff, 0, DFS_BUFF_SIZE);
+		strcpy(buff, static_files[i]);
+		for (int j = 0; j < file_len; j++)
+			_program_page(file_pt + j, buff + j * NAND_PAGE_SIZE);
+	}
 }
 
 int dfs_mount(void) {
