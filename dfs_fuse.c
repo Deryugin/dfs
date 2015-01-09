e888bf2d654d99749668702a8cd024d0c6ad8d09
Deryugin <deryugin.denis@gmail.com>
2015-01-10 00:13:50 +0300

    FUSE reading files

--- dfs_fuse.c
+++ dfs_fuse.c
@@ -11,25 +11,32 @@
 #include <string.h>
 #include <errno.h>
 
+static struct file_desc* fd_from_path(const char *path) {
+	for (int i = 0; i < DFS_INODES_MAX; i++) {
+		struct file_desc *fd = dfs_open(i);
+		if (!fd)
+			break;
+		if (strcmp(path, fd->node->name) == 0)
+			return fd;
+	}
+
+	return NULL;
+}
+
 int dfs_fuse_getattr (const char * path, struct stat *buf) {
-	printf("dfs_fuse_getattr\n");
+	memset(buf, 0, sizeof(struct stat));
 	
 	if (strcmp(path, "/") == 0) {
-		buf->st_mode = S_IFDIR | 0755;
+		buf->st_mode = S_IFDIR | 0777;
 		buf->st_nlink = 2;
 		return 0;
 	} else {
-		path++;
-		for (int i = 0; i < DFS_INODES_MAX; i++) {
-			struct file_desc *fd = dfs_open(i);
-			if (!fd)
-				break;
-			if (strcmp(path, fd->node->name) == 0) {
-				buf->st_mode = S_IFREG | 0444;
-				buf->st_nlink = 2;
-				buf->st_size = 1;
-				return 0;
-			}
+		struct file_desc *fd = fd_from_path(path + 1);
+		if (fd && strcmp(path + 1, fd->node->name) == 0) {
+			buf->st_mode = S_IFREG | 0777;
+			buf->st_nlink = 1;
+			buf->st_size = 8;
+			return 0;
 		}
 	}
 	
@@ -37,19 +44,30 @@ int dfs_fuse_getattr (const char * path, struct stat *buf) {
 }
 
 int dfs_fuse_open (const char *path, struct fuse_file_info *fi) {
-	printf("dfs_fuse_open\n");
 	return 0;
 }
 
-int dfs_fuse_read (const char *path, char *buf, size_t suze,
+char st[] = "Hello!QWEQWEQWEQWE";
+int dfs_fuse_read (const char *path, char *buf, size_t size,
 			off_t offset, struct fuse_file_info *fi) {
-	printf("dfs_fuse_read\n");
-	return 0;
+	
+	size_t len;
+	(void) fi;
+	struct file_desc *fd = fd_from_path(path + 1);
+	fd->pos = offset;
+	len = strlen(st);
+	if (offset < len) {
+		if (offset + size > len)
+			size = len - offset;
+		dfs_read(fd, buf, size);
+	} else
+		size = 0;
+
+	return size;
 }
 
 int dfs_fuse_readdir (const char *path, void *buf, fuse_fill_dir_t filler,
 				off_t offset, struct fuse_file_info *fi) {
-	printf("dfs_fuse_readdir\n");
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
 	
